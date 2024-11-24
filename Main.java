410a80551d95ab8b3c884907281b1dd7ff49715d
snakyv <v03032006@stud.op.edu.ua>
2024-11-24 05:13:53 +0200

    Added method for calculating the sum of two numbers

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        // Виклик нового методу для обчислення суми
+        int result = sum(5, 10);
+        System.out.println("Sum: " + result);
+    }
+
+    // Метод для обчислення суми двох чисел
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
+
+
